ffa18e323182ee6ac63cc4f4d09e5a629651652d
lspb007 <lspb_007@163.com>
2020-11-03 09:46:43 +0800

    调整备案

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
     </div>
     <router-view/>
     <div class="ali-report-wrap">
-      
-      <a class="ace-link ace-link-primary ali-report-link" href="http://www.beian.gov.cn/portal/registerSystemInfo?spm=a2cmq.17630005.J_9220772140.109.7ef879fe4wvUYd" target="_blank" data-spm-anchor-id="a2cmq.17630005.J_9220772140.109"><img data-src="//img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png" class="ali-report-img" src="//img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png"><span class="ali-report-link-text" data-spm-anchor-id="a2cmq.17630005.J_9220772140.i0.7ef879fe4wvUYd">京ICP备19035303号</span></a></div>
+      <!-- 网站备案 -->
+      <!-- <a class="ace-link ace-link-primary ali-report-link" href="http://www.beian.gov.cn/portal/registerSystemInfo?spm=a2cmq.17630005.J_9220772140.109.7ef879fe4wvUYd" target="_blank" data-spm-anchor-id="a2cmq.17630005.J_9220772140.109"><img data-src="//img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png" class="ali-report-img" src="//img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png"><span class="ali-report-link-text" data-spm-anchor-id="a2cmq.17630005.J_9220772140.i0.7ef879fe4wvUYd">京ICP备19035303号</span></a></div> -->
   </div>
 </template>
 
